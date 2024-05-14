4a2bd13528e000874d1f6f1e2f2a5eb152178a07
vmourtia <valentinmourtialon@protonmail.com>
2024-05-14 20:46:44 +0200

    Permission issue with wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-wp cli update --yes --allow-root
+wp-cli.phar cli update --yes --allow-root
 
-wp core download --allow-root --path=/var/www/html/wordpress
+wp-cli.phar core download --allow-root --path=/var/www/html/wordpress
 
-wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_USER} --dbpass=${WP_USER_PWD} --dbhost="mariadb":"3306" --path=/var/www/html/wordpress --allow-root
+wp-cli.phar config create --dbname=${WP_DB_NAME} --dbuser=${WP_USER} --dbpass=${WP_USER_PWD} --dbhost="mariadb":"3306" --path=/var/www/html/wordpress --allow-root
 
-wp core install --url=vmourtia.42.fr/wordpress --title=wordpress --admin_user=${WP_DB_ADMIN_USERNAME} --admin_password=${WP_DB_ADMIN_PWD} --admin_email=${WP_DB_ADMIN_EMAIL} --path=/var/www/html/wordpress --allow-root
+wp-cli.phar core install --url=vmourtia.42.fr/wordpress --title=wordpress --admin_user=${WP_DB_ADMIN_USERNAME} --admin_password=${WP_DB_ADMIN_PWD} --admin_email=${WP_DB_ADMIN_EMAIL} --path=/var/www/html/wordpress --allow-root
 
-wp user create ${WP_DB_USER} ${WP_DB_USER_EMAIL} --user_pass=${WP_DB_USER_PWD} --role=editor --display_name=${WP_DB_USER} --path=/var/www/html/wordpress --allow-root
+wp-cli.phar user create ${WP_DB_USER} ${WP_DB_USER_EMAIL} --user_pass=${WP_DB_USER_PWD} --role=editor --display_name=${WP_DB_USER} --path=/var/www/html/wordpress --allow-root
+
